8d94be30e7869b7c8ccfcc592fb5c0b72131b137
Jonathan Kingston <jonathan@jooped.co.uk>
2019-05-24 03:42:50 +0100

    Add functionality for ui to toggle the proxy state

--- popup.js
+++ popup.js
@@ -1,27 +1,44 @@
-console.log(1);
-
-async function handle() {
+async function init() {
   const tabInfo = await browser.runtime.sendMessage({
     type: "tabInfo",
   });
-  console.log("response", tabInfo);
   if (tabInfo && "proxied" in tabInfo) {
     setProxiedState(tabInfo.proxied)
   } else {
     setProxiedState(null)
   }
+
+  let {enabledState} = await browser.storage.local.get(["enabledState"]) || true;
+
+  const toggleProxy = document.getElementById("toggle-proxy");
+  showButtonState(toggleProxy, enabledState);
+  addActiveListener(toggleProxy, (e) => {
+    enabledState = !enabledState;
+    browser.storage.local.set({
+      enabledState
+    });
+    showButtonState(toggleProxy, enabledState);
+  });
+}
+
+function showButtonState(el, state) {
+  el.textContent = state ? "Disable Proxy" : "Enable Proxy";
+}
+
+function addActiveListener(el, listener) {
+  el.addEventListener("click", listener);
+  el.addEventListener("submit", listener);
 }
 
 function setProxiedState(state) {
-  let message = document.createElement("div");
   let stateName = "Not proxied";
   if (state) {
     stateName = "Proxied";
   } else if (state === null) {
     stateName = "Indeterminate";
   }
+  const message = document.getElementById("state");
   message.textContent = `Proxy state: ${stateName}`;
-  document.body.appendChild(message);
 }
 
-handle();
+init();
