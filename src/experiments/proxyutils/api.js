c99e57758708529b47c47142368ca98c20e8a723
Andrea Marchesini <amarchesini@mozilla.com>
2019-06-27 18:45:49 +0300

    make lint happy

--- src/experiments/proxyutils/api.js
+++ src/experiments/proxyutils/api.js
@@ -4,7 +4,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-/* globals ChromeUtils, ExtensionAPI, ExtensionCommon, Services */
+/* globals ChromeUtils, ExtensionAPI, ExtensionCommon, Services, Ci */
 
 "use strict";
 
