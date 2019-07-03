4813760bd839bc103d414c1cc488e7502e1ad781
Andrea Marchesini <amarchesini@mozilla.com>
2019-07-03 12:06:51 +0300

    Fixed a typo

--- src/commons.js
+++ src/commons.js
@@ -7,5 +7,5 @@ const PROXY_STATE_INACTIVE = "inactive";
 // The user is registered, the proxy is active.
 const PROXY_STATE_ACTIVE = "active";
 
-// The is another proxy in use.
+// There is another proxy in use.
 const PROXY_STATE_OTHERINUSE = "otherInUse";
