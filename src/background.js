54100d75412bfa1a37fbee13bdcdb30ee1f5433b
Andrea Marchesini <amarchesini@mozilla.com>
2019-07-10 12:18:26 +0300

    Manage your account (#112)

--- src/background.js
+++ src/background.js
@@ -646,6 +646,10 @@ class Background {
         case "goBack":
           this.updateUI();
           break;
+
+        case "manageAccount":
+          this.manageAccount();
+          break;
       }
     });
 
@@ -681,6 +685,13 @@ class Background {
       this.proxyState = PROXY_STATE_OFFLINE;
       this.updateUI();
     }
+  }
+
+  manageAccount() {
+    let contentServer = this.fxaEndpoints.get(FXA_ENDPOINT_ISSUER);
+    browser.tabs.create({
+      url: contentServer + "/settings",
+    })
   }
 }
 
--- src/views/settings.js
+++ src/views/settings.js
@@ -10,13 +10,22 @@ class ViewSettings extends View {
     return escapedTemplate`
       <ul class="settingsLinks">
         <li><strong>${this.getTranslation("accountLabel")}</strong><span class="end">${data.userInfo.email}</span></li>
-        <li><a href="https://support.mozilla.org/en-US/">${this.getTranslation("manageAccountLink")}</a></li>
-        <li><a href="https://support.mozilla.org/en-US/">${this.getTranslation("helpAndSupportLink")}</a></li>
-        <li><a href="https://support.mozilla.org/en-US/">${this.getTranslation("privacyPolicyLink")}</a></li>
-        <li><a href="https://support.mozilla.org/en-US/">${this.getTranslation("termsAndConditionsLink")}</a></li>
+        <li><a href="#" id="manageAccountAnchor">${this.getTranslation("manageAccountLink")}</a></li>
+        <li><a href="TODO">${this.getTranslation("helpAndSupportLink")}</a></li>
+        <li><a href="TODO">${this.getTranslation("privacyPolicyLink")}</a></li>
+        <li><a href="TODO">${this.getTranslation("termsAndConditionsLink")}</a></li>
       </ul>
-      <button class="primary">${this.getTranslation("manageAccountButton")}</button>
+      <button class="primary" id="manageAccount">${this.getTranslation("manageAccountButton")}</button>
     `;
+  }
+
+  handleEvent(e) {
+    if (e.target.id == "manageAccount" || e.target.id == "manageAccountAnchor") {
+      View.sendMessage("manageAccount");
+      e.preventDefault();
+      close();
+      return;
+    }
   }
 }
 
