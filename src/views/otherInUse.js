d403d60e3cd1c7f70c1c842a5746e3e4db69a0b8
Andrea Marchesini <amarchesini@mozilla.com>
2019-07-12 16:54:41 +0300

    Separate view for otherInUse

---
+++ src/views/otherInUse.js
@@ -0,0 +1,20 @@
+import {View} from '../view.js'
+import {escapedTemplate} from '../template.js'
+
+class ViewOtherInUse extends View {
+  show() {
+    View.setState("disabled", this.getTranslation("heroProxyOff"));
+    View.hideToggleButton();
+
+    return escapedTemplate`
+    <p>
+      ${this.getTranslation("viewOtherInUse")}
+    </p>`;
+  }
+}
+
+const view = new ViewOtherInUse();
+const name = "otherInUse";
+
+View.registerView(view, name);
+export default name;
