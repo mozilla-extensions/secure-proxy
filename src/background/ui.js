7516b4eb71884308a67401a87ce8ad350c11c9c7
Andrea Marchesini <amarchesini@mozilla.com>
2019-08-16 15:54:02 +0300

    Don't show toasts at startup time (#447)

--- src/background/ui.js
+++ src/background/ui.js
@@ -273,10 +273,13 @@ export class UI extends Component {
     }
   }
 
-  async update() {
+  async update(showToast = true) {
+    if (showToast) {
+      await this.showStatusPrompt();
+    }
+
     await Promise.all([
       this.updateIcon(),
-      this.showStatusPrompt(),
       this.sendDataToCurrentPort(),
     ]);
   }
