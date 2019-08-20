d1e61284415fa897202b8196a97e79e528b39a66
Andrea Marchesini <amarchesini@mozilla.com>
2019-08-20 08:44:26 +0300

    Test for well-known-data component

--- src/background/tester.js
+++ src/background/tester.js
@@ -1,7 +1,31 @@
 /* eslint-disable verify-await/check */
 
+import {WellKnownData} from "./wellKnownData.js";
+
 const tests = [
+  {
+    name: "WellKnownData",
+    run: testWellKnownData,
+  },
 ];
+
+async function testWellKnownData() {
+  const wkd = new WellKnownData();
+  wkd.init({value: {}});
+
+  Tester.is(wkd.hasWellKnownData(), false, "No data initially");
+  Tester.is(wkd.isAuthUrl("wow"), false, "IsAuthUrl works also without data");
+  Tester.is(wkd.isAuthUrl("https://accounts.firefox.com"), true, "IsAuthUrl works also without data");
+  Tester.is(wkd.excludedDomains().length, 0, "ExcludedDomains returns an empty array");
+
+  // Now, let's fetch data.
+  Tester.is(await wkd.getIssuerEndpoint(), "https://accounts.firefox.com", "issuer URL");
+  Tester.is(await wkd.getProfileEndpoint(), "https://profile.accounts.firefox.com/v1/profile", "profile URL");
+  Tester.is(await wkd.getTokenEndpoint(), "https://oauth.accounts.firefox.com/v1/token", "token URL");
+  Tester.is(wkd.excludedDomains().length, 3, "ExcludedDomains returns something");
+  Tester.is(wkd.isAuthUrl("https://profile.accounts.firefox.com"), true, "IsAuthUrl works better with data");
+  Tester.is(wkd.isAuthUrl("https://oauth.accounts.firefox.com"), true, "IsAuthUrl works better with data");
+}
 
 export class Tester {
   static async run(main) {
