ef8a7ac0bbae9bb22e6c44520ca079d28f77dca9
Honza Bambas :mayhemer <honzab@firemni.cz>
2019-09-06 18:43:10 +0200

    570, set proxy.onRequest callback only when necessary

--- src/background/connection.js
+++ src/background/connection.js
@@ -1,14 +1,22 @@
 const CONNECTION_TIMEOUT = 20000; // 20 secs.
 
 export class ConnectionTester {
-  static run() {
+  static run(receiver) {
     log("executing a fetch to check the connection");
 
-    return new Promise((resolve, reject) => {
+    const proxyListener = receiver.syncHandleEvent("proxyRequestCallback");
+
+    let promise = new Promise((resolve, reject) => {
       setTimeout(_ => reject(), CONNECTION_TIMEOUT);
 
+      // Make sure to go via the proxy for this and only this request
+      browser.proxy.onRequest.addListener(proxyListener, {
+        urls: [CONNECTING_HTTP_REQUEST]
+      });
+      log("added proxy.onRequest listener for", CONNECTING_HTTP_REQUEST);
+
       // eslint-disable-next-line verify-await/check
-      fetch(CONNECTING_HTTP_REQUEST, { cache: "no-cache"}).
+      fetch(CONNECTING_HTTP_REQUEST, { cache: "no-cache" }).
         then(r => {
           if (r.status === 200) {
             // eslint-disable-next-line verify-await/check
@@ -24,5 +32,13 @@ export class ConnectionTester {
           reject();
         });
     });
+
+    // Make sure we remove the proxy listener either the promise resolves or rejects
+    promise.catch(_ => { }).then(_ => {
+      browser.proxy.onRequest.removeListener(proxyListener);
+      log("removed proxy.onRequest listener for", CONNECTING_HTTP_REQUEST);
+    });
+
+    return promise;
   }
 }
